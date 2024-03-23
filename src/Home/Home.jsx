f48ecb1749cf7d1a7806382763d1ec3d465bf0dc
shakil ahmed <shakiltha338@gmail.com>
2024-03-24 01:01:48 +0600

    added categoryList and banner

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Banner from "../components/Banner/Banner";
+import CategoryList from "../components/CategoryList/CategoryList";
+
 const Home = () => {
   return (
     <div>
+      <Banner />
+      <CategoryList />
       <h2>This is Home Component</h2>
     </div>
   );
